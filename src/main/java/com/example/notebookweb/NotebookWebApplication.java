31c1d5000d51452f950d2bb37e0969da0a492bd3
UkrainskiyS <ukarinskiys@gmail.com>
2022-01-21 16:15:24 +0300

    init repo

---
+++ src/main/java/com/example/notebookweb/NotebookWebApplication.java
@@ -0,0 +1,12 @@
+package com.example.notebookweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebookWebApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/notebookweb/NotebookWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notebookweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
